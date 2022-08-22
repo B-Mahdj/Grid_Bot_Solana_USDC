abca8c78bb755e8ae6ca1625b56718688c76b073
Bilal Mahdjoubi <bilal.mahdjoubi95@gmail.com>
2022-08-22 08:36:45 +0200

    Fix for price update and sell/buy actions

--- src/gridBot.ts
+++ src/gridBot.ts
@@ -8,7 +8,8 @@ const slippage = +process.env.SLIPPAGE;
 const DECIMALS_PER_USDC = +process.env.DECIMALS_PER_USDC;
 var numberOfSells = 0;
 var numberOfBuys = 0;
-
+var amountOfSolToSell = 0;
+var amountOfUSDCToSell = 0;
 
 const app = express();
 app.listen(port, () => console.log(`App listening on port ${port}!`));
@@ -16,19 +17,23 @@ launch();
 
 export async function launch() {
     const { solanaWallet, orders } = await setup();
+    amountOfSolToSell = await getAmountSolToUse();
+    amountOfUSDCToSell = await getAmountOfUSDCToSell();
+
     var buyOrders = orders.buyOrders;
     var sellOrders = orders.sellOrders;
 
     // Create a function that loop itself when it is finished
     while (true) {
         // Get the actual price of 1 SOL vs USDC
-        var [solanaPrice, bestRoute] = await getSolanaPriceAndBestRoute();
+        var solanaPrice = await getSolanaPriceFor1SOL();
         console.log("price: " + solanaPrice);
 
         // If the price is equals or below the lowest buy order inside the array, buy the coin
         if (solanaPrice <= buyOrders[0]) {
             console.log("Buying solana at price", solanaPrice);
-            //console.log("Best route found is:", bestRoute);
+            const [price, bestRoute] = await getSolanaPriceAndBestRouteToBuySol(amountOfUSDCToSell);
+            console.log("Using " + amountOfUSDCToSell + " USDC to buy" + price + " SOL");
             await buySolana(bestRoute);
             buyOrders.splice(0, 1);
             // Update the sell orders array with a new sell order at the start of the array
@@ -39,8 +44,9 @@ export async function launch() {
 
         // If the price is equals or above the lowest sell order, sell the coin
         if (solanaPrice >= sellOrders[0]) {
-            console.log("Selling at price", solanaPrice);
-            //console.log("Best route found is:", bestRoute);
+            console.log("Selling solana at price", solanaPrice);
+            const [price, bestRoute] = await getSolanaPriceAndBestRouteToSellSol(amountOfSolToSell);
+            console.log("Using " + amountOfSolToSell + " SOL to sell" + price + " USDC");
             await sellSolana(bestRoute);
             sellOrders.splice(0, 1);
             // Update the buy orders array with a new buy order at the start of the array
@@ -58,7 +64,19 @@ export async function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function getSolanaPriceAndBestRoute(): Promise<[number, [any]]> {
+async function getSolanaPriceFor1SOL(): Promise<number> {
+    var amount = LAMPORTS_PER_SOL;
+    const { data } = await (
+        await axios.get(
+            `https://quote-api.jup.ag/v1/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=${amount}&slippage=${slippage}`
+        )
+    ).data;
+    const routes = data;
+    return routes[0].outAmount / DECIMALS_PER_USDC;
+}
+
+
+async function getSolanaPriceAndBestRouteToSellSol(amount:number): Promise<[number, [any]]> {
     var amount = LAMPORTS_PER_SOL;
     const { data } = await (
         await axios.get(
@@ -69,24 +87,41 @@ async function getSolanaPriceAndBestRoute(): Promise<[number, [any]]> {
     return [(routes[0].outAmount / DECIMALS_PER_USDC), routes[0]];
 }
 
-export async function getAmountSolToUse(): Promise<number> {
+async function getSolanaPriceAndBestRouteToBuySol(amount:number): Promise<[number, [any]]> {
+    const { data } = await (
+        await axios.get(
+            `https://quote-api.jup.ag/v1/quote?inputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&outputMint=So11111111111111111111111111111111111111112&amount=${amount}&slippage=${slippage}`
+        )
+    ).data;
+    const routes = data;
+    return [(routes[0].outAmount / LAMPORTS_PER_SOL), routes[0]];
+}
+
+async function getAmountSolToUse(): Promise<number> {
     return 0;
 }
 
 
+async function getAmountOfUSDCToSell(): Promise<number> {
+    return 0;
+}
 
 async function buySolana(price: any[]): Promise<void> {
-    numberOfSells++;
-    console.log("Number of sells:", numberOfSells);
-    // Get the number of SOL to use
-    var amount = await getAmountSolToUse();
+    numberOfBuys++;
+    console.log("Number of buys:", numberOfBuys);
+
+    // Make the buy order
+
+    // Update the amount of sol/ USDC to use for next order
 }
 
 
 async function sellSolana(price: any[]): Promise<void> {
-    numberOfBuys++;
-    console.log("Number of buys:", numberOfBuys);
+    numberOfSells++;
+    console.log("Number of sells:", numberOfSells);
 
-    // Get the number of SOL to use
-    var amount = await getAmountSolToUse();
+    // Make the sell order
+
+    // Update the amount of sol/ USDC to use for next order
 }
+
