9405c029e394cc9db74f152a23415605a3ac387e
Bilal Mahdjoubi <bilal.mahdjoubi95@gmail.com>
2022-08-28 16:17:47 +0200

    [BUGFIX] Stop using SOL and start using WSOL

--- src/getTokens.ts
+++ src/getTokens.ts
@@ -2,7 +2,7 @@ import { Connection, GetProgramAccountsFilter, LAMPORTS_PER_SOL, PublicKey } fro
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { Wallet } from "@project-serum/anchor";
 
-export async function getTokenAccounts(wallet: string, solanaConnection: Connection): Promise<number> {
+export async function getUSDCBalance(wallet: string, solanaConnection: Connection): Promise<number> {
     var tokenBalanceUSDC: number = 0;
     const filters:GetProgramAccountsFilter[] = [
         {
@@ -36,11 +36,40 @@ export async function getTokenAccounts(wallet: string, solanaConnection: Connect
     return tokenBalanceUSDC;
 }
 
+export async function getWSolBalance(wallet: string, solanaConnection: Connection): Promise<number> {
+  var tokenBalanceWSol: number = 0;
+  const filters:GetProgramAccountsFilter[] = [
+      {
+        dataSize: 165,    //size of account (bytes)
+      },
+      {
+        memcmp: {
+          offset: 32,     //location of our query in the account (bytes)
+          bytes: wallet,  //our search criteria, a base58 encoded string
+        },            
+      }];
+  const accounts = await solanaConnection.getParsedProgramAccounts(
+      TOKEN_PROGRAM_ID, 
+      {filters: filters}
+  );
+  console.log(`Found ${accounts.length} token account(s) for wallet ${wallet}.`);
+  accounts.forEach((account, i) => {
+      //Parse the account data
+      const parsedAccountInfo:any = account.account.data;
+      const mintAddress:string = parsedAccountInfo["parsed"]["info"]["mint"];
+      const tokenBalance: number = parsedAccountInfo["parsed"]["info"]["tokenAmount"]["uiAmount"];
+      if(mintAddress === "So11111111111111111111111111111111111111112") {
+        tokenBalanceWSol = tokenBalance;
+      }
+  });
+  return tokenBalanceWSol;
+}
+
 
 export async function getAmountSolToSell(wallet:Wallet, solanaConnection: Connection): Promise<number> {
-  const publicKey = new PublicKey(wallet.publicKey);
-  var balance =  await solanaConnection.getBalance(publicKey);
-  var amountSolToSell = (balance / LAMPORTS_PER_SOL) * +process.env.PERCENTAGE_OF_BALANCE_TO_SELL;
+  var wsolBalance = await getWSolBalance(wallet.publicKey.toString(),solanaConnection);
+  console.log("WSol Balance", wsolBalance);
+  var amountSolToSell = wsolBalance * +process.env.PERCENTAGE_OF_BALANCE_TO_SELL;
   if(amountSolToSell < +process.env.MIN_AMOUNT_SOL_TO_SELL) {
     return 0;
   }
@@ -51,7 +80,7 @@ export async function getAmountSolToSell(wallet:Wallet, solanaConnection: Connec
 
 
 export async function getAmountOfUSDCToSell(wallet:Wallet, solanaConnection: Connection): Promise<number> {
-  var usdcBalance = await getTokenAccounts(wallet.publicKey.toString(),solanaConnection);
+  var usdcBalance = await getUSDCBalance(wallet.publicKey.toString(),solanaConnection);
   console.log("Usdc Balance", usdcBalance);
   var amountOfUSDCToSell = usdcBalance * +process.env.PERCENTAGE_OF_USDC_TO_SELL;
   if(amountOfUSDCToSell < +process.env.MIN_AMOUNT_USDC_TO_SELL) {
